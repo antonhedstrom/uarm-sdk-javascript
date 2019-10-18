9e275be3fd4fb4616efb6268b22ef5a0b03d2baa
Anton Hedström <antonhedstrom@gmail.com>
2019-10-18 21:59:26 +0200

    Added np config and exporting SDK in index.

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const findPort = require('./comm/find-port');
 const portRegexp = /Arduino/i;
 
 module.exports = {
+  uArmSDK,
   findPort: (acceptFn) => {
     if (!acceptFn) {
       acceptFn = (port) => portRegexp.test(port.manufacturer);
     }
     return findPort({ acceptFn });
   },
-  uArmSDK,
 };
